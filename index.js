c26a7f4db1fe3acd2aefd35ed82350adc88ca23c
Josell Vibar <vibarjosell@gmail.com>
2022-10-21 20:23:47 +0800

    add node modules

--- index.js
+++ index.js
@@ -5,16 +5,17 @@
 // npm init - *** will create package.json ***
 // npm install express
 // add .gitignore
+//9f7e9e0b314b24c7c6eaf82ebc88862a - API KEY (openweatherAPP)
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen('port',() => {
-    console.log(`server is running on port ${port}`);
+app.get('/', (req, res) => {
+    res.send('Hello WOrld!!');
+
 })
 
-app.get('/',(req, res) => {
-    res.send('Hello WOrld!!');
-    
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
 })
