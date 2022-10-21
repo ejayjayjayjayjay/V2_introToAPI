5ee97f1a8485c99262604bc1dfc5238f77fe85a6
Josell Vibar <vibarjosell@gmail.com>
2022-10-21 19:42:27 +0800

    add index.js and package.json

---
+++ index.js
@@ -0,0 +1,19 @@
+#STEPS IN INSTALLING EXPRESS
+// mkdir myapp
+// cd myapp
+// create repository
+// npm init - *** will create package.json ***
+// npm install express
+// add .gitignore
+
+const express = require('express') 
+const app = express() 
+const port = 3000
+
+app.get('/', (req, res) => { 
+    res.send('Hello World!') 
+})
+
+app.listen(port, () => { 
+    console.log(Example app listening on port ${port}) 
+})
